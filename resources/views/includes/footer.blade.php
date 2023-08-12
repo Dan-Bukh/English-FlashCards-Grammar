7cc10f127752d5cbe74e90f7ae98e843f27b02bd
Daniel <daniilanatol9@gmail.com>
2023-08-12 14:17:47 -0600

    Start Project and done header and footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="pt-3 mt-4 justify-content-end border-top">
+    <p class="text-center text-body-secondary">© 2023 EnglishGrammar, Inc</p>
+</footer>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,20 @@
+<header class="py-3">
+    <div class="container">
+        <nav class="navbar navbar-expand-md fixed-top bg-body-tertiary border-bottom" >
+            <div class="container-md">
+    
+              <a class="navbar-brand fst-italic ps-3" href="#">EnglishGrammar</a>
+              <div>
+                <ul class="navbar-nav fw-medium mx-5">
+                  <li class="nav-item fs-5 mx-3">
+                    <a class="nav-link active" aria-current="page" href="#">Грамматика</a>
+                  </li>
+                  <li class="nav-item fs-5 mx-3">
+                    <a class="nav-link" href="#">Новые Слова</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </div>
+</header>
